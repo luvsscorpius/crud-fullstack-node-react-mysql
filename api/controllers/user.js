5cd2b78920fc084fbb3076094f7d3da8172f924f
and <andersonvittor1801@gmail.com>
2023-10-07 16:31:43 -0300

    feat(grid/api): creating component grid and finishing backend

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -11,3 +11,47 @@ export const getUsers = (_, res) => {
         return res.status(200).json(data)
     })
 }
+
+export const addUser = (req, res) => {
+    const query = "INSERT INTO usuarios (nome, email, fone, data_nascimento) VALUES (?)"
+
+    const values = [
+        req.body.nome,
+        req.body.email,
+        req.body.fone,
+        req.body.data_nascimento
+    ]
+
+    db.query(query, [values], (err) => {
+        if (err) return res.json(err)
+
+        return res.status(200).json("Usuário criado com sucesso")
+    })
+}
+
+export const updateUser = (req, res) => {
+    const query = "UPDATE usuarios SET nome = ?, email = ?, fone = ?, data_nascimento =? WHERE id = ?"
+
+    const values = [
+        req.body.nome,
+        req.body.email,
+        req.body.fone,
+        req.body.data_nascimento
+    ]
+
+    db.query(query, [...values, req.params.id], (err) => {
+        if (err) return res.json(err)
+
+        return res.status(200).json("Usuário atualizado com sucesso")
+    })
+}
+
+export const deleteUser = (req, res) => {
+    const query = "DELETE FROM usuarios WHERE id = ?"
+
+    db.query(query, [req.params.id], (err) => {
+        if (err) return res.json(err)
+
+        return res.status(200).json("Usuário deletado com sucesso")
+    })
+}
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,9 @@ import styled from 'styled-components';
 import Form from './components/Form.jsx'
 import { toast, ToastContainer } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css"
+import Grid from './components/Grid';
+import { useEffect, useState } from 'react'
+import axios from "axios"
 
 const Container = styled.div`
   width: 100%;
@@ -17,11 +20,29 @@ const Container = styled.div`
 const Title = styled.h2``
 
 function App() {
+
+  const [users, setUsers] = useState([])
+  const [onEdit, setOnEdit] = useState(null)
+
+  const getUsers = async () => {
+    try {
+      const res = await axios.get("http://localhost:8800/")
+      setUsers(res.data.sort((a, b) => (a.nome > b.nome ? 1 : -1)))
+    } catch (error) {
+      toast.error(error)
+    }
+  }
+
+  useEffect(() => {
+    getUsers()
+  }, [setUsers])
+
   return (
     <>
       <Container>
         <Title>Usuários</Title>
-        <Form />
+        <Form onEdit={onEdit} setOnEdit={setOnEdit} getUsers={getUsers} />
+        <Grid users={users} setUsers={setUsers} setOnEdit={setOnEdit} />
       </Container>
       <ToastContainer autoClose={3000} position={toast.POSITION.BOTTOM_LEFT} />
       <GlobalStyle />
